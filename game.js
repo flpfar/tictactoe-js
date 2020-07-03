59163175c31a77b3de974d1aa621ab64f9440214
shubhsk88 <shubham88singh@gmail.com>
2020-07-03 22:08:00 +0530

    Making the functionality

--- game.js
+++ game.js
@@ -6,24 +6,43 @@ const gameBoard = (() => {
   };
 })();
 
+function player(name, char, arr) {
+  const chances = [
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 3, 6],
+    [2, 4, 6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+  const winner = () => {
+    return chances.some((chance) => chance.every((num) => arr.includes(num)));
+  };
+  //x=[];o=[];
+  //player1=player("sam",'X',x);
+  //player2=player('jask','O',o);
+  ele.textContent=player.char;
+//if(Char=="X") playe1.x.push(indexOf('X)
+// )
+// char='O'
+//if(char==='o') player2.arr.push(grid.indexof())
+//char='x
+
+  return {
+    name,
+    char,
+    arr,
+    winner,
+  };
+}
+
 const displayController = (() => {
-  const boardGrid = document.querySelector('.board-grid');
-  const boardItems = document.querySelectorAll('.item');
+  const boardGrid = document.querySelector(".board-grid");
+  const boardItems = document.querySelectorAll(".item");
   const render = () => {
     let i = 0;
-    while (i < 8) {
-      if (i % 2 === 0) {
-        boardItems[i].addEventListener('click', () => {
-          boardItems[i].textContent = 'X';
-          i += 1;
-        });
-      } else {
-        boardItems[i].addEventListener('click', () => {
-          boardItems[i].textContent = 'O';
-          i += 1;
-        });
-      }
-    }
   };
   return { render };
 })();
