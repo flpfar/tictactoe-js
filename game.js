3745f0e11fcbddd4fa4a91fc2118e524b6c6eb46
shubhsk88 <shubham88singh@gmail.com>
2020-07-03 21:17:50 +0530

    Updating the tictoe logic

--- game.js
+++ game.js
@@ -2,22 +2,30 @@ const gameBoard = (() => {
   const gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   return {
-    gameboard
-  }
+    gameboard,
+  };
 })();
 
 const displayController = (() => {
   const boardGrid = document.querySelector('.board-grid');
   const boardItems = document.querySelectorAll('.item');
   const render = () => {
-    for(let i = 0; i < boardItems.length; i++){
-      boardItems.addEventListener('click', () => {
-        boardItems[i].textContent = 'X'
-      })
-      boardItems[i].innerHTML = gameBoard.gameboard[i];
+    let i = 0;
+    while (i < 8) {
+      if (i % 2 === 0) {
+        boardItems[i].addEventListener('click', () => {
+          boardItems[i].textContent = 'X';
+          i += 1;
+        });
+      } else {
+        boardItems[i].addEventListener('click', () => {
+          boardItems[i].textContent = 'O';
+          i += 1;
+        });
+      }
     }
-  }
-  return {render}
+  };
+  return { render };
 })();
 
 const user = (name) => {
