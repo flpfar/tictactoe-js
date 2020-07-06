e7abb42ad423fbfd3cd14bad0ac0ad022768cbc1
Felipe Rosa <flp.far@gmail.com>
2020-07-06 19:08:17 -0300

    Add display controller

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
 /* eslint-disable */
 
-const gameboard = (() => {
-  const arr = new Array(9).fill('');
-  const boardContainer = document.querySelector('.board-container');
-  const boardItems = document.querySelectorAll('.item');
-  turn = 0;
-  for (let i = 0; i < boardItems.length; i++) {
-    boardItems[i].addEventListener('click', function () {
-      console.log(arr);
-      if (arr[i] === '') {
-          if(turn%2===0)
-          {
-              arr[i]='X';
-              boardItems[i].innerHTML = '<div class="text-red-500"><i class="fas fa-dog"></i></div>';
-          }
-          else{
-              arr[i]='O';
-              boardItems[i].innerHTML = '<div class="text-white"><i class="fas fa-cat"></i></div>'
-          }
-          turn++;
-      }
-    });
+const gameBoard = (() => {
+  const board = new Array(9).fill('');
+  
+  let turn = 0;
+
+  function addTurn() {
+    turn += 1;
   }
+
+  function getTurn() {
+    return turn;
+  }
+  return {board, addTurn, getTurn}
 })();
 
-gameboard;
+const displayController = (gameBoard) => {
+  const boardItems = document.querySelectorAll('.item');
+  const render = () => {
+    for(let i = 0; i < boardItems.length; i++){
+      if(gameBoard.board[i] !== '') {
+        boardItems[i].innerHTML = (gameBoard.board[i] == 'X') ? '<div class="text-red-500"><i class="fas fa-dog"></i></div>' : '<div class="text-white"><i class="fas fa-cat"></i></div>';
+      }
+    }
+  }
+
+  const startListeners = () => {
+    for (let i = 0; i < boardItems.length; i++) {
+      boardItems[i].addEventListener('click', function () {
+        if (gameBoard.board[i] === '') {
+            if(gameBoard.getTurn()%2===0)
+            {
+              gameBoard.board[i]='X';
+            }
+            else{
+              gameBoard.board[i]='O';
+            }
+            gameBoard.addTurn();
+            render();
+        }
+      });
+    }
+  }
+  return {render, startListeners}
+};
+
+const player = (name) => {
+  return {name}
+};
+
+const game = () => {
+  const display = displayController(gameBoard);
+  display.startListeners();
+
+}
+
+game()
